29d4b47f94eb6b56b93c7c03f197d321613ba502
Lima X <35961248+Lima-X-Coding@users.noreply.github.com>
2020-05-07 23:54:02 +0200

    intermediat

--- XOrCryptEx.cpp
+++ XOrCryptEx.cpp
@@ -4,40 +4,92 @@
 #include <Windows.h>
 #include <malloc.h>
 
-void XOrCryptE(void* pData, int nDatalen, void* key) {
+// Random Generator for seeding and providing random data
+static uint64_t s[4];
+static uint64_t x;
+static inline uint64_t rotl(const uint64_t x, int k) {
+	return (x << k) | (x >> (64 - k));
+}
+uint64_t next(void) {
+	const uint64_t result = rotl(s[1] * 5, 7) * 9;
+	const uint64_t t = s[1] << 17;
+
+	s[2] ^= s[0];
+	s[3] ^= s[1];
+	s[1] ^= s[2];
+	s[0] ^= s[3];
+	s[2] ^= t;
+	s[3] = rotl(s[3], 45);
+
+	return result;
+}
+uint64_t nextsm() {
+	uint64_t z = (x += 0x9e3779b97f4a7c15);
+	z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+	z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+	return z ^ (z >> 31);
+}
+
+
+
+#define KEY_SIZE 2048
+void XOrEncrypt(void* pData, int nDatalen, void* key) {
 	char x;
-	for (int i = 0; i < nDatalen / 32; i++) {
-		for (int j = 0; j < 32; j++) {
-			x = ((char*)pData)[(i * 32) + j];
-			((char*)pData)[(i * 32) + j] ^= ((char*)key)[j];
-			((char*)key)[j] ^= x;
-		}
-	} for (int j = 0; j < nDatalen % 32; j++) {
-		((char*)pData)[((nDatalen / 32) * 32) + j] ^= ((char*)key)[j];
+	for (int i = 0; i < nDatalen; i++) {
+		x = ((char*)pData)[i];
+		((char*)pData)[i] ^= ((char*)key)[i % (KEY_SIZE / 8)];
+		((char*)key)[i % (KEY_SIZE / 8)] ^= x;
+	}
+}
+void XOrDecrypt(void* pData, int nDatalen, void* key) {
+	for (int i = 0; i < nDatalen; i++) {
+		((char*)pData)[i] ^= ((char*)key)[i % (KEY_SIZE / 8)];
+		((char*)key)[i % (KEY_SIZE / 8)] ^= ((char*)pData)[i];
 	}
 }
 
-void XOrCryptD(void* pData, int nDatalen, void* key) {
-	for (int i = 0; i < nDatalen / 32; i++) {
-		for (int j = 0; j < 32; j++) {
-			((char*)pData)[(i * 32) + j] ^= ((char*)key)[j];
-			((char*)key)[j] ^= ((char*)pData)[(i * 32) + j];
-		}
-
-	} for (int j = 0; j < nDatalen % 32; j++) {
-		((char*)pData)[((nDatalen / 32) * 32) + j] ^= ((char*)key)[j];
-	}
-}
+#define MB 1048576
+#define MUL 1024
 
 int main() {
-	void* data = (char*)malloc(100);
-	memcpy(data, "Hello, this is a test to prove a concept of XorCrypt\nIt has now been extendet to test it fully", 95);
-	void* key = (char*)malloc(32);
-	memcpy(key, "5623321233257612323762163715637", 32);
+	// Seed
+	x = 18357827323532641321;
+	for (int i = 0; i < 4; i++)
+		s[i] = nextsm();
 
-	XOrCryptE(data, 95, key);
-	memcpy(key, "5623321233257612323762163715637", 32);
-	XOrCryptD(data, 95, (char*)key);
+	// Random data stream
+	void* data = (char*)malloc(MB * MUL);
+	for (int i = 0; i < (MB * MUL) / 8; i++)
+		((uint64_t*)data)[i] = next();
+
+	// Copy for comparing
+	void* data2 = (char*)malloc(MB * MUL);
+	memcpy(data2, data, MB * MUL);
+
+	// Key
+	void* key = (char*)malloc(256);
+	memcpy(key, "dfawuziijdhag76cdv 2ubcaol25627b1necmnhbuw7a8bc9dw6v9w0adwrbc32657onrr"
+		"g67c3b8oamnre6c3cobw672c gr5ohiabco682erc8b6t37c6377rlcwvk09ä0öv,ceg763nm9n8bw"
+		"nad8wn6a7o823,8rm7923bv8r376bc7267n8rzxbga78nb28c6r8cnj2edcbbgm8,9l09xa9owcavn"
+		"z2vrm87cgqm,xe9chi8vw3cr3vcted", 256);
+
+	// Encrypt and reset key
+	XOrEncrypt(data, MB * MUL, key);
+	memcpy(key, "dfawuziijdhag76cdv 2ubcaol25627b1necmnhbuw7a8bc9dw6v9w0adwrbc32657onrr"
+		"g67c3b8oamnre6c3cobw672c gr5ohiabco682erc8b6t37c6377rlcwvk09ä0öv,ceg763nm9n8bw"
+		"nad8wn6a7o823,8rm7923bv8r376bc7267n8rzxbga78nb28c6r8cnj2edcbbgm8,9l09xa9owcavn"
+		"z2vrm87cgqm,xe9chi8vw3cr3vcted", 256);
+
+	// Decrypt
+	XOrDecrypt(data, MB * MUL, key);
+
+	int a = memcmp(data, data2, MB * MUL);
+
+	free(data);
+	free(data2);
+	free(key);
+
+	return a;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
