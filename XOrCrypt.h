a18b1b769ff54f3a23ca9b6cc4c84df8d4848568
Lima X <35961248+Lima-X-Coding@users.noreply.github.com>
2020-05-08 16:58:57 +0200

    v1.0

--- XOrCrypt.h
+++ XOrCrypt.h
@@ -23,7 +23,7 @@
     e.g. on x64 (8-Bytes) : BLOCK_SIZE = 64
             x86 (4-Bytes) : BLOCK_SIZE = 32
 */
-#define _XC_BLOCKSIZE 8
+#define _XC_BLOCKSIZE 32
 
 /////////////////////////////////////////////////////////////////////////////////////
 
@@ -42,7 +42,30 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    /* Function Declarations */
+    /*  Function Declarations
+        pData    : Pointer to the data you want to either encrypt or decrypt
+        nDataLen : Lenght of data to be encrypted/decrypted (count in Bytes).
+                   The maximum length of a datablock that can be encrypted/decrypted
+                   is dependend on the targets architectures size.
+                   If you still want to encrypt/decrypt data that is larger then the
+                   maximum value that the targets architectures size can handle, you
+                   have to feed in the data as single groups of the size of half that
+                   what the targets architectures integerlimit is.
+                   e.g. on 8-Bit the group size would have to be 128 bytes.
+
+                   The folling code is an example on how to achive that, it is only useful for 8/16-Bit:
+                   | for (int i = 0; i < nDataLen / (1 << ((sizeof(_XC_DT) * 8) - 1)); i++)              |
+		           |     fnXOrCrypt((void*)((uintX_t)pData + (i * (1 << ((sizeof(_XC_DT) * 8) - 1)))),   |
+			       |         (1 << ((sizeof(_XC_DT) * 8) - 1)), key);                                    |
+	               | fnXOrCrypt((void*)((uintX_t)pData + ((nDataLen / (1 << ((sizeof(_XC_DT) * 8) - 1))) |
+                   |     * (1 << ((sizeof(_XC_DT) * 8) - 1)))),                                          |
+                   |     nDataLen % (1 << ((sizeof(_XC_DT) * 8) - 1)), key);                             |
+
+        pKey     : Pointer to key (array) that you want to use to encrypt/decrypt
+                   data.
+                   Keep a copy of the original key as the key you pass to the
+                   functions will be modified !
+    */
     void fnXOrEncrypt(void* pData, _XC_DT nDataLen, void* pKey);
     void fnXOrDecrypt(void* pData, _XC_DT nDataLen, void* pKey);
 
--- XOrCryptEx.cpp
+++ XOrCryptEx.cpp
@@ -6,8 +6,7 @@
 #include "XOrCrypt.h"
 
 // Random Generator for seeding and providing random data
-static uint64_t s[4];
-static uint64_t x;
+static uint64_t s[5];
 static inline uint64_t rotl(const uint64_t x, int k) {
 	return (x << k) | (x >> (64 - k));
 }
@@ -25,19 +24,20 @@ uint64_t next(void) {
 	return result;
 }
 uint64_t nextsm() {
-	uint64_t z = (x += 0x9e3779b97f4a7c15);
+	uint64_t z = (s[4] += 0x9e3779b97f4a7c15);
 	z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
 	z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
 	return z ^ (z >> 31);
 }
 
-#define MB 32000
-//#define MB 1048576
-#define MUL 1
+// #define MB 32036
+#define MB 1048576
+#define MUL 512
+
 
 int main() {
-	// Seed
-	x = 18357827323532641321;
+	/* Seed Xoshiro //////// Used for debuging / tesing *///////////////////////////////
+	s[4] = 18357827323532641321;
 	for (int i = 0; i < 4; i++)
 		s[i] = nextsm();
 
@@ -50,18 +50,19 @@ int main() {
 	void* data2 = (char*)malloc(MB * MUL);
 	memcpy(data2, data, MB * MUL);
 
-	// Key
+	/* Key *////////////////////////////////////////////////////////////////////////////
 	void* key = (char*)malloc(256);
 	memcpy(key, "dfawuziijdhag76cdv 2ubcaol25627b1necmnhbuw7a8bc9dw6v9w0adwrbc32657onrr"
 		"g67c3b8oamnre6c3cobw672c gr5ohiabco682erc8b6t37c6377rlcwvk09ä0öv,ceg763nm9n8bw"
 		"nad8wn6a7o823,8rm7923bv8r376bc7267n8rzxbga78nb28c6r8cnj2edcbbgm8,9l09xa9owcavn"
 		"z2vrm87cgqm,xe9chi8vw3cr3vcted", 256);
 
-
-	for (int i = 0; i < MB / 128; i++) {
-		fnXOrEncrypt((void*)((uint64_t)data + (i * 128)), 128, key);
-	}
-	fnXOrEncrypt((void*)((uint64_t)data + ((MB / 128) * 128)), MB % 128, key);
+	// Stream encryption
+	for (int i = 0; i < MB / (1 << ((sizeof(_XC_DT) * 8) - 1)); i++)
+		fnXOrEncrypt((void*)((uint32_t)data + (i * (1 << ((sizeof(_XC_DT) * 8) - 1)))),
+			(1 << ((sizeof(_XC_DT) * 8) - 1)), key);
+	fnXOrEncrypt((void*)((uint32_t)data + ((MB / (1 << ((sizeof(_XC_DT) * 8) - 1)))
+		* (1 << ((sizeof(_XC_DT) * 8) - 1)))), MB % (1 << ((sizeof(_XC_DT) * 8) - 1)), key);
 
 	// Encrypt and reset key
 //	fnXOrEncrypt(data, MB * MUL, key);
@@ -71,13 +72,13 @@ int main() {
 		"z2vrm87cgqm,xe9chi8vw3cr3vcted", 256);
 
 	// Decrypt
-	for (int i = 0; i < MB / 128; i++) {
-		fnXOrDecrypt((void*)((uint64_t)data + (i * 128)), 128, key);
-	}
-	fnXOrDecrypt((void*)((uint64_t)data + ((MB / 128) * 128)), MB % 128, key);
+	for (int i = 0; i < MB / (1 << ((sizeof(_XC_DT) * 8) - 1)); i++)
+		fnXOrDecrypt((void*)((uint32_t)data + (i * (1 << ((sizeof(_XC_DT) * 8) - 1)))),
+			(1 << ((sizeof(_XC_DT) * 8) - 1)), key);
+	fnXOrDecrypt((void*)((uint32_t)data + ((MB / (1 << ((sizeof(_XC_DT) * 8) - 1)))
+		* (1 << ((sizeof(_XC_DT) * 8) - 1)))), MB % (1 << ((sizeof(_XC_DT) * 8) - 1)), key);
 
 //	fnXOrDecrypt(data, MB * MUL, key);
-
 	int a = memcmp(data, data2, MB * MUL);
 
 	free(data);
@@ -86,14 +87,3 @@ int main() {
 
 	return a;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started:
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
